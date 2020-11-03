a028a0a512b309d1de43f75f97b07c530428f7e4
hiroyukitamura <hirotamu3@gmail.com>
2020-11-03 20:36:20 +0900

    [proto][screen_intro] introduce Hive

---
+++ lib/di/hive_client.dart
@@ -0,0 +1,18 @@
+import 'package:hive/hive.dart';
+
+class HiveClient {
+
+  static bool isInitialLaunchApp() => _HivePrefClient._isInitialLaunchApp();
+
+  static void setInitialLaunchApp() => _HivePrefClient._setInitialLaunchApp();
+}
+
+class _HivePrefClient {
+  static const _NAME = 'PREFERENCE';
+
+  static const _KEY_INITIAL_LAUNCH_APP = 'INITIAL_LAUNCH_APP';
+
+  static bool _isInitialLaunchApp() => Hive.box(_NAME).get(_KEY_INITIAL_LAUNCH_APP, defaultValue: true);
+
+  static void _setInitialLaunchApp() => Hive.box(_NAME).put(_KEY_INITIAL_LAUNCH_APP, false);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:shirasu/di/hive_client.dart';
 import 'package:shirasu/resource/styles.dart';
+import 'package:shirasu/screen_dashboard/screen_dashboard.dart';
 import 'package:shirasu/screen_intro/screen_intro.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) => MaterialApp(
+  Widget build(BuildContext context) {
+    final child = true ? ScreenIntro() : ScreenDashboard();
+    return MaterialApp(
       title: 'Flutter Demo',
       theme: Styles.theme,
-      home: ScreenIntro(),
+      home: Navigator(pages: [
+        MaterialPage(child: child)
+      ], onPopPage: (route, result) {
+        return route.didPop(result);
+      },),
     );
+  }
 }

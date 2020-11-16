2cffecb9cded9e642ee424d757f341f17c9033c1
hiroyukitamura <hirotamu3@gmail.com>
2020-11-16 20:35:46 +0900

    [dev] create api model for watch history

---
+++ lib/model/watch_history_data.g.dart
@@ -0,0 +1,39 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'watch_history_data.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_WatchHistoriesData _$_$_WatchHistoriesDataFromJson(
+    Map<String, dynamic> json) {
+  return _$_WatchHistoriesData(
+    viewerUser: json['viewerUser'] == null
+        ? null
+        : ViewerUser.fromJson(json['viewerUser'] as Map<String, dynamic>),
+  );
+}
+
+Map<String, dynamic> _$_$_WatchHistoriesDataToJson(
+        _$_WatchHistoriesData instance) =>
+    <String, dynamic>{
+      'viewerUser': instance.viewerUser,
+    };
+
+_$_ViewerUserSimple _$_$_ViewerUserSimpleFromJson(Map<String, dynamic> json) {
+  return _$_ViewerUserSimple(
+    watchHistories: json['watchHistories'] == null
+        ? null
+        : WatchHistories.fromJson(
+            json['watchHistories'] as Map<String, dynamic>),
+    typename: json['__typename'] as String,
+  );
+}
+
+Map<String, dynamic> _$_$_ViewerUserSimpleToJson(
+        _$_ViewerUserSimple instance) =>
+    <String, dynamic>{
+      'watchHistories': instance.watchHistories,
+      '__typename': instance.typename,
+    };

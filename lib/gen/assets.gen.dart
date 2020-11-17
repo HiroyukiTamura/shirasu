68c9b41b5d3e055a22adf889c5dc1b315e0948de
KazamaMac <hiroyuktamura@gmail.com>
2020-11-17 15:19:23 +0900

    [dev] introduce flutter gen

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,120 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/services.dart';
+
+class Assets {
+  Assets._();
+
+  static const SvgGenImage logoOfficial =
+      SvgGenImage('assets/logo_official.svg');
+  static const SvgGenImage logoOfficialShadow =
+      SvgGenImage('assets/logo_official_shadow.svg');
+  static const SvgGenImage undrawNotifyRe65on =
+      SvgGenImage('assets/undraw_Notify_re_65on.svg');
+  static const SvgGenImage undrawMyAppGrf2 =
+      SvgGenImage('assets/undraw_my_app_grf2.svg');
+  static const SvgGenImage undrawSecurityO890 =
+      SvgGenImage('assets/undraw_security_o890.svg');
+}
+
+class AssetGenImage extends AssetImage {
+  const AssetGenImage(String assetName)
+      : _assetName = assetName,
+        super(assetName);
+  final String _assetName;
+
+  Image image({
+    Key key,
+    ImageFrameBuilder frameBuilder,
+    ImageLoadingBuilder loadingBuilder,
+    ImageErrorWidgetBuilder errorBuilder,
+    String semanticLabel,
+    bool excludeFromSemantics = false,
+    double width,
+    double height,
+    Color color,
+    BlendMode colorBlendMode,
+    BoxFit fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    FilterQuality filterQuality = FilterQuality.low,
+  }) {
+    return Image(
+      key: key,
+      image: this,
+      frameBuilder: frameBuilder,
+      loadingBuilder: loadingBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      width: width,
+      height: height,
+      color: color,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      filterQuality: filterQuality,
+    );
+  }
+
+  String get path => _assetName;
+}
+
+class SvgGenImage {
+  const SvgGenImage(this._assetName);
+
+  final String _assetName;
+
+  SvgPicture svg({
+    Key key,
+    bool matchTextDirection = false,
+    AssetBundle bundle,
+    String package,
+    double width,
+    double height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    WidgetBuilder placeholderBuilder,
+    Color color,
+    BlendMode colorBlendMode = BlendMode.srcIn,
+    String semanticsLabel,
+    bool excludeFromSemantics = false,
+    Clip clipBehavior = Clip.hardEdge,
+  }) {
+    return SvgPicture.asset(
+      _assetName,
+      key: key,
+      matchTextDirection: matchTextDirection,
+      bundle: bundle,
+      package: package,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      color: color,
+      colorBlendMode: colorBlendMode,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      clipBehavior: clipBehavior,
+    );
+  }
+
+  String get path => _assetName;
+}

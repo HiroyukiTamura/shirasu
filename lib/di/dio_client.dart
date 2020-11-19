11e95f0d8fe417ff7e5d5e4bc2322a84e431b70a
KazamaMac <hiroyuktamura@gmail.com>
2020-11-19 21:35:00 +0900

    [dev] create api client for auth token renewal

--- lib/di/dio_client.dart
+++ lib/di/dio_client.dart
@@ -1,11 +1,13 @@
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:shirasu/model/result_token_refresh.dart';
 import 'package:shirasu/model/signed_cookie_result.dart';
 import 'package:shirasu/model/video_type.dart';
 
 class DioClient {
   final Dio _dio = Dio();
+  static const _URL_OAUTH_TOKEN = 'https://shirasu.io/oauth/token';
   static const _URL_SIGNED_COOKIE =
       'https://video.shirasu.io/get-signed-cookie';
 
@@ -22,8 +24,7 @@ class DioClient {
         }));
 
     final success = SignedCookieResult.fromJson(response.data).ok;
-    if (!success)
-      throw Exception('getSignedCookie failed : ${response.data}');
+    if (!success) throw Exception('getSignedCookie failed : ${response.data}');
 
     return response.headers[HttpHeaders.setCookieHeader].map((it) {
       final cookie = Cookie.fromSetCookieValue(it);
@@ -40,5 +41,22 @@ class DioClient {
       default:
         throw Exception('unknown video type $videoType');
     }
+  }
+
+
+  /// auth0 API doc: https://auth0.com/docs/tokens/refresh-tokens/use-refresh-tokens
+  Future<void> requestRenewToken(String clientId, String refreshToken) async {
+    final result = await _dio.post<Map<String, dynamic>>(
+      _URL_OAUTH_TOKEN,
+      data: {
+        'grant_type': 'refresh_token',
+        'client_id': clientId,
+        'refresh_token': refreshToken,
+      },
+      options: Options(headers: {
+        HttpHeaders.contentTypeHeader: 'application/x-www-form-urlencoded'
+      }),
+    );
+    return ResultTokenRefresh.fromJson(result.data);
   }
 }

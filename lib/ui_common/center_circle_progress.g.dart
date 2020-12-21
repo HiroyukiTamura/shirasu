1cb9ac8bcd35d6f0bdcbfbb85bade11dd301e279
hiroyukitamura <hiroyuktamura@gmail.com>
2020-12-22 01:27:21 +0900

    SHIR-8 # comment [dev][ui_common] refactoring with functional_widget

---
+++ lib/ui_common/center_circle_progress.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'center_circle_progress.dart';
+
+// **************************************************************************
+// FunctionalWidgetGenerator
+// **************************************************************************
+
+class CenterCircleProgress extends StatelessWidget {
+  const CenterCircleProgress({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext _context) => centerCircleProgress();
+}
---
+++ lib/ui_common/empty_list_widget.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'empty_list_widget.dart';
+
+// **************************************************************************
+// FunctionalWidgetGenerator
+// **************************************************************************
+
+/// @see [PageError]
+class EmptyListWidget extends StatelessWidget {
+  /// @see [PageError]
+  const EmptyListWidget({Key key, @required this.text, @required this.icon})
+      : super(key: key);
+
+  /// @see [PageError]
+  final String text;
+
+  /// @see [PageError]
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext _context) =>
+      emptyListWidget(text: text, icon: icon);
+}

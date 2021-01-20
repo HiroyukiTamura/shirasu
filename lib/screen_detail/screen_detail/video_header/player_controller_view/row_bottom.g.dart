547f1282aa811a8d0245c58dacc75b6bb93cc7db
hiroyukitamura <hiroyuktamura@gmail.com>
2021-01-21 08:18:54 +0900

    SHIR-4 # comment [dev][screen_detail] 1. refactor 2. remove `BetterPlayerConfiguration.thumbnail` 3. relayout UI for horizontal orientation

---
+++ lib/screen_detail/screen_detail/video_header/player_controller_view/row_bottom.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'row_bottom.dart';
+
+// **************************************************************************
+// FunctionalWidgetGenerator
+// **************************************************************************
+
+class RowBottom extends HookWidget {
+  const RowBottom({Key key, @required this.id}) : super(key: key);
+
+  final String id;
+
+  @override
+  Widget build(BuildContext _context) => rowBottom(id: id);
+}
+
+class _TimeText extends HookWidget {
+  const _TimeText(
+      {Key key, @required this.id, this.padding = const EdgeInsets.all(8)})
+      : super(key: key);
+
+  final String id;
+
+  final EdgeInsets padding;
+
+  @override
+  Widget build(BuildContext _context) => _timeText(id: id, padding: padding);
+}

0b32a53cd3554979d4148b728c869237f10471fb
hiroyukitamura <hiroyuktamura@gmail.com>
2021-01-29 10:11:00 +0900

    SHIR-24 # comment [dev][page_comment] editing portal

---
+++ lib/portal/slide_in_from_btm.g.dart
@@ -0,0 +1,32 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'slide_in_from_btm.dart';
+
+// **************************************************************************
+// FunctionalWidgetGenerator
+// **************************************************************************
+
+class SlideInFromBtm extends StatelessWidget {
+  const SlideInFromBtm(
+      {Key key,
+      @required this.child,
+      @required this.visible,
+      @required this.duration,
+      @required this.btmSheetContent})
+      : super(key: key);
+
+  final Widget child;
+
+  final bool visible;
+
+  final Duration duration;
+
+  final Widget btmSheetContent;
+
+  @override
+  Widget build(BuildContext _context) => slideInFromBtm(
+      child: child,
+      visible: visible,
+      duration: duration,
+      btmSheetContent: btmSheetContent);
+}

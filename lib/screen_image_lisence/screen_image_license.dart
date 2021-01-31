072fabbd1a09d47d5d9e2cdc406d1aa5affc4e18
hiroyukitamura <hiroyuktamura@gmail.com>
2021-02-01 02:02:20 +0900

    SHIR-27 # comment [dev][screen_image_license] fix error handling on `Util.launchUrl`

--- lib/screen_image_lisence/screen_image_license.dart
+++ lib/screen_image_lisence/screen_image_license.dart
@@ -2,11 +2,27 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:functional_widget_annotation/functional_widget_annotation.dart';
+import 'package:hooks_riverpod/all.dart';
 import 'package:shirasu/di/url_util.dart';
+import 'package:shirasu/resource/dimens.dart';
 import 'package:shirasu/resource/strings.dart';
+import 'package:shirasu/ui_common/msg_ntf_listener.dart';
 import 'package:shirasu/util.dart';
+import 'package:shirasu/viewmodel/message_notifier.dart';
 
 part 'screen_image_license.g.dart';
+
+final _kPrvDetailSnackBarMsgNotifier =
+    StateNotifierProvider.autoDispose<SnackBarMessageNotifier>(
+        (ref) => SnackBarMessageNotifier());
+
+final _kPrvDetailSnackMsg = Provider.autoDispose<SnackData>((ref) {
+  final state = ref.watch(_kPrvDetailSnackBarMsgNotifier.state);
+  return SnackData(
+    state.snackMsg,
+    Dimens.SNACK_BAR_DEFAULT_MARGIN,
+  );
+});
 
 @swidget
 Widget screenImageLicense() => SafeArea(
@@ -14,10 +30,13 @@ Widget screenImageLicense() => SafeArea(
         appBar: AppBar(
           title: const Text(Strings.ITEM_TITLE_IMAGE_LICENCE),
         ),
-        body: ListView(
-          children: const [
-            _ListTileRobot(),
-          ],
+        body: SnackEventListener(
+          provider: _kPrvDetailSnackMsg,
+          child: ListView(
+            children: const [
+              _ListTileRobot(),
+            ],
+          ),
         ),
       ),
     );
@@ -28,45 +47,48 @@ class _ListTileRobot extends StatelessWidget {
   static const _TEXT_STYLE = TextStyle(fontSize: 18);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) => ListTile(
+        title: RichText(
+          text: TextSpan(
+            style: _TEXT_STYLE,
+            children: [
+              _autoLink(
+                context: context,
+                text: 'Roboto',
+                url: UrlUtil.URL_IMG_LICENSE_ROBOTO,
+              ),
+              const TextSpan(text: ' by '),
+              _autoLink(
+                context: context,
+                text: 'Icons Mind',
+                url: UrlUtil.URL_IMG_LICENSE_ICONS_MIND,
+              ),
+            ],
+          ),
+        ),
+      );
+
+  TextSpan _autoLink({
+    @required BuildContext context,
+    @required String text,
+    @required String url,
+  }) {
     final linkStyle = TextStyle(
       color: Theme.of(context).accentColor,
       decoration: TextDecoration.underline,
     );
 
-    return ListTile(
-      title: RichText(
-        text: TextSpan(
-          style: _TEXT_STYLE,
-          children: [
-            TextSpan(
-              text: 'Roboto',
-              style: linkStyle,
-              recognizer: TapGestureRecognizer()
-                ..onTap = () async => _onTapRoboto(context),
+    return TextSpan(
+      text: text,
+      style: linkStyle,
+      recognizer: TapGestureRecognizer()
+        ..onTap = () async => Util.launchUrl(
+              context,
+              url,
+              () => context
+                  .read(_kPrvDetailSnackBarMsgNotifier)
+                  .notifyMsg(const SnackMsg.unknown(), false),
             ),
-            const TextSpan(text: ' by '),
-            TextSpan(
-              text: 'Icons Mind',
-              style: linkStyle,
-              recognizer: TapGestureRecognizer()
-                ..onTap = () async => _onTapIconsMind(context),
-            ),
-          ],
-        ),
-      ),
     );
   }
-
-  Future<void> _onTapRoboto(BuildContext context) async =>
-      Util.launchUrl(context, UrlUtil.URL_IMG_LICENSE_ROBOTO, () {
-        //todo
-        throw UnimplementedError();
-      });
-
-  Future<void> _onTapIconsMind(BuildContext context) async =>
-      Util.launchUrl(context, UrlUtil.URL_IMG_LICENSE_ICONS_MIND, () {
-        //todo
-        throw UnimplementedError();
-      });
 }
--- lib/ui_common/msg_ntf_listener.dart
+++ lib/ui_common/msg_ntf_listener.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:functional_widget_annotation/functional_widget_annotation.dart';
 import 'package:hooks_riverpod/all.dart';
 import 'package:shirasu/main.dart';
+import 'package:shirasu/resource/dimens.dart';
 import 'package:shirasu/util.dart';
 import 'package:shirasu/viewmodel/message_notifier.dart';
 

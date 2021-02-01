fc1b7a577f72040a1bd849bfb1ee07eab0c6af7b
hiroyukitamura <hiroyuktamura@gmail.com>
2021-02-02 04:31:43 +0900

    [dev] apply `functional_widget` to user_location_dialog.dart

--- lib/dialog/user_location_dialog.dart
+++ lib/dialog/user_location_dialog.dart
@@ -1,28 +1,29 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:functional_widget_annotation/functional_widget_annotation.dart';
 import 'package:hooks_riverpod/all.dart';
 import 'package:shirasu/client/local_json_client.dart';
 import 'package:shirasu/resource/strings.dart';
 import 'package:shirasu/viewmodel/viewmodel_user_location_dialog.dart';
 import 'package:shirasu/extension.dart';
 
+part 'user_location_dialog.g.dart';
+
 final _viewModelProvider =
     StateNotifierProvider.autoDispose<ViewModelUserLocationDialog>(
   (ref) => ViewModelUserLocationDialog.createFromSettingVm(ref),
 );
 
-class MsgUserLocation {
-  const MsgUserLocation(this.countryCode, this.prefectureCode);
+class _MsgUserLocation {
+  const _MsgUserLocation(this.countryCode, this.prefectureCode);
 
   final String countryCode;
   final String prefectureCode;
 }
 
-//todo migrate to functional_widget
 class UserLocationDialog extends HookWidget {
-
-  const UserLocationDialog();
+  const UserLocationDialog._();
 
   @override
   Widget build(BuildContext context) => AlertDialog(
@@ -40,7 +41,7 @@ class UserLocationDialog extends HookWidget {
             onPressed: () {
               final data = context.read(_viewModelProvider.state).data;
               final result =
-                  MsgUserLocation(data.countryCode, data.prefectureCode);
+                  _MsgUserLocation(data.countryCode, data.prefectureCode);
               Navigator.pop(context, result);
             },
             child: const Text(Strings.DIALOG_OK),
@@ -48,68 +49,55 @@ class UserLocationDialog extends HookWidget {
         ],
       );
 
-  static Future<MsgUserLocation> show(BuildContext context) async => showDialog(
-      context: context, builder: (context) => const UserLocationDialog());
+  static Future<_MsgUserLocation> show(BuildContext context) async => showDialog(
+      context: context, builder: (context) => const UserLocationDialog._());
 }
 
-class _UserLocationDialogContent extends HookWidget {
-  const _UserLocationDialogContent();
-
-  @override
-  Widget build(BuildContext context) {
-    final data = useProvider(_viewModelProvider.state.select((it) => it.data));
-    return Container(
-      child: Column(mainAxisSize: MainAxisSize.min, children: [
-        const _RowItemCountry(),
-        if (LocalJsonClient.isJapan(data.countryCode)) ...[
-          const SizedBox(height: 24),
-          const _RowItemPrefecture(),
-        ]
-      ]),
-    );
-  }
+@hwidget
+Widget _userLocationDialogContent() {
+  final data = useProvider(_viewModelProvider.state.select((it) => it.data));
+  return Container(
+    child: Column(mainAxisSize: MainAxisSize.min, children: [
+      const _RowItemCountry(),
+      if (LocalJsonClient.isJapan(data.countryCode)) ...[
+        const SizedBox(height: 24),
+        const _RowItemPrefecture(),
+      ]
+    ]),
+  );
 }
 
-class _RowItemPrefecture extends HookWidget {
-  const _RowItemPrefecture({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final data =
-        useProvider(_viewModelProvider.state.select((state) => state.data));
-    return DropdownButton<String>(
-      isExpanded: true,
-      value: data.prefectureCode,
-      items: data.prefectureData.prefecture
-          .map((prefecture) => DropdownMenuItem<String>(
-                value: prefecture.code,
-                child: Text(prefecture.name),
-              ))
-          .toUnmodifiable(),
-      onChanged: (code) =>
-          context.read(_viewModelProvider).changePrefecture(code),
-    );
-  }
+@hwidget
+Widget _rowItemPrefecture(BuildContext context) {
+  final data =
+      useProvider(_viewModelProvider.state.select((state) => state.data));
+  return DropdownButton<String>(
+    isExpanded: true,
+    value: data.prefectureCode,
+    items: data.prefectureData.prefecture
+        .map((prefecture) => DropdownMenuItem<String>(
+              value: prefecture.code,
+              child: Text(prefecture.name),
+            ))
+        .toUnmodifiable(),
+    onChanged: (code) =>
+        context.read(_viewModelProvider).changePrefecture(code),
+  );
 }
 
-class _RowItemCountry extends HookWidget {
-  const _RowItemCountry({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final data =
-        useProvider(_viewModelProvider.state.select((state) => state.data));
-    return DropdownButton<String>(
-      isExpanded: true,
-      value: data.countryCode,
-      items: data.countryData.countries.entries
-          .map((entry) => DropdownMenuItem<String>(
-                value: entry.key,
-                child: Text(entry.value),
-              ))
-          .toUnmodifiable(),
-      onChanged: (code) =>
-          context.read(_viewModelProvider).changeCountry(code),
-    );
-  }
+@hwidget
+Widget _rowItemCountry(BuildContext context) {
+  final data =
+      useProvider(_viewModelProvider.state.select((state) => state.data));
+  return DropdownButton<String>(
+    isExpanded: true,
+    value: data.countryCode,
+    items: data.countryData.countries.entries
+        .map((entry) => DropdownMenuItem<String>(
+              value: entry.key,
+              child: Text(entry.value),
+            ))
+        .toUnmodifiable(),
+    onChanged: (code) => context.read(_viewModelProvider).changeCountry(code),
+  );
 }

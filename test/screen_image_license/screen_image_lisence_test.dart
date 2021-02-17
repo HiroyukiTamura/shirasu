ee4f6a9216f562f73a88ee8d43bc97ef0e36ec32
hiroyukitamura <hiroyuktamura@gmail.com>
2021-02-17 09:16:21 +0900

    SHIR-45 # comment [test] create widget  test for `ScreenImageLicense`

---
+++ test/screen_image_license/screen_image_lisence_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shirasu/screen_image_lisence/screen_image_license.dart';
+
+import '../widget_test_util/test_runner_base.dart';
+
+Future<void> main() async {
+
+  final runner = TestRunnerBase(() => const ScreenImageLicense());
+  await runner.init();
+
+  group('ScreenImageTest', () {
+    runner.testGoldensSimple(testName: 'normal', overrides: []);
+  });
+}

4c882d8970f606796f8a7a55b6f57b260465aa4b
hiroyukitamura <hiroyuktamura@gmail.com>
2021-02-22 22:36:00 +0900

    SHIR-56 # comment [dev] add model class and samples

---
+++ lib/model/socket/connection_ack.g.dart
@@ -0,0 +1,33 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'connection_ack.json';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_ConnectionAck _$_$_ConnectionAckFromJson(Map<String, dynamic> json) {
+  return _$_ConnectionAck(
+    type: json['type'] as String,
+    payload: json['payload'] == null
+        ? null
+        : Payload.fromJson(json['payload'] as Map<String, dynamic>),
+  );
+}
+
+Map<String, dynamic> _$_$_ConnectionAckToJson(_$_ConnectionAck instance) =>
+    <String, dynamic>{
+      'type': instance.type,
+      'payload': instance.payload,
+    };
+
+_$_Payload _$_$_PayloadFromJson(Map<String, dynamic> json) {
+  return _$_Payload(
+    connectionTimeoutMs: json['connectionTimeoutMs'] as int,
+  );
+}
+
+Map<String, dynamic> _$_$_PayloadToJson(_$_Payload instance) =>
+    <String, dynamic>{
+      'connectionTimeoutMs': instance.connectionTimeoutMs,
+    };

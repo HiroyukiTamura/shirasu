31988487790393adf54995f44a6515478e5bd3d4
HiroyukTamura <hirotamu3@gmail.com>
2021-04-12 11:16:32 +0900

    SHIR-77 # full implement UI and BL

--- lib/screen_fcm/screen_fcm.dart
+++ lib/screen_fcm/screen_fcm.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:functional_widget_annotation/functional_widget_annotation.dart';
 import 'package:shirasu/model/lr.dart';
 import 'package:shirasu/repository/url_util.dart';
+import 'package:shirasu/resource/dimens.dart';
 import 'package:shirasu/resource/strings.dart';
 import 'package:shirasu/resource/styles.dart';
 import 'package:shirasu/router/global_route_path.dart';
@@ -12,67 +13,82 @@ import 'package:shirasu/ui_common/hive_fcm_topic_listenable.dart';
 import 'package:dartx/dartx.dart';
 import 'package:shirasu/ui_common/movie_list_item.dart';
 import 'package:shirasu/extension.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shirasu/ui_common/msg_ntf_listener.dart';
+import 'package:shirasu/viewmodel/message_notifier.dart';
+import 'package:shirasu/viewmodel/viewmodel_fcm.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 part 'screen_fcm.g.dart';
 
-class ScreenFcm extends StatefulWidget {
+final _kPrvViewModel = StateNotifierProvider.autoDispose<ViewModelFcm>((ref) => ViewModelFcm(ref.read));
+
+final kPrvFcmSnackBar =
+    StateNotifierProvider.autoDispose<SnackBarMessageNotifier>(
+        (ref) => SnackBarMessageNotifier());
+
+final _kPrvDetailSnackMsg =
+    Provider.autoDispose<SnackData>((ref) {
+  final snackMsgEvent = ref.watch(kPrvFcmSnackBar.state);
+  return SnackData(snackMsgEvent.snackMsg, Dimens.SNACK_BAR_DEFAULT_MARGIN);
+});
+
+class ScreenFcm extends HookWidget {
+
   const ScreenFcm();
 
-  @override
-  _ScreenFcmState createState() => _ScreenFcmState();
-}
-
-class _ScreenFcmState extends State<ScreenFcm> {
   @override
   Widget build(BuildContext context) => SafeArea(
         child: Scaffold(
           appBar: AppBar(
             title: const Text(Strings.APP_BAR_SCREEN_FCM),
           ),
-          body: HiveFcmTopicListenable(
-            builder: (context, topic, widget) {
-              if (topic.isEmpty)
-                return const EmptyListWidget(
-                  text: Strings.EMPTY_FCM,
-                  icon: Icons.notifications,
+          body: SnackEventListener(
+            provider: _kPrvDetailSnackMsg,
+            child: HiveFcmTopicListenable(
+              builder: (context, topic, widget) {
+                if (topic.isEmpty)
+                  return const EmptyListWidget(
+                    text: Strings.EMPTY_FCM,
+                    icon: Icons.notifications,
+                  );
+
+                List<Widget> children = [];
+                if (topic.subscribingChannels.isNotEmpty) {
+                  children.add(const _Title(text: 'チャンネル'));
+                  children += topic.subscribingChannels.values
+                      .map((item) => _ChannelListTile(
+                            id: item.id,
+                            title: item.name,
+                            onDismissed: (_) =>
+                                _onDismissChannel(context, item.id),
+                          ))
+                      .toList();
+                }
+
+                if (topic.subscribingPrograms.isNotEmpty) {
+                  children.add(const _Title(text: '番組'));
+                  children += topic.subscribingPrograms.values
+                      .where((it) => DateTime.now() < it.broadcastAt)
+                      .map(
+                        (it) => _Dismissible(
+                            keyValue: 'program_${it.id}',
+                            onDismissed: (_) => _onDismissPrg(context, it.id),
+                            child: MovieListItem(
+                              id: it.id,
+                              title: it.title,
+                              broadcastAt: it.broadcastAt,
+                              onTap: () => _onTapProgram(context, it.id),
+                            )),
+                      )
+                      .toList();
+                }
+
+                return ListView(
+                  children: children,
                 );
-
-              List<Widget> children = [];
-              if (topic.subscribingChannels.isNotEmpty) {
-                children.add(const _Title(text: 'チャンネル'));
-                children += topic.subscribingChannels.values
-                    .mapIndexed((i, item) => _ChannelListTile(
-                          id: item.id,
-                          title: item.name,
-                          onDismissed: (direction) =>
-                              _onDismissChannel(direction, item.id),
-                        ))
-                    .toList();
-              }
-
-              if (topic.subscribingPrograms.isNotEmpty) {
-                children.add(const _Title(text: '番組'));
-                children += topic.subscribingPrograms.values
-                    .where((it) => DateTime.now() < it.broadcastAt)
-                    .mapIndexed(
-                      (i, it) => _Dismissible(
-                          keyValue: 'program_${it.id}',
-                          onDismissed: (direction) =>
-                              _onDismissPrg(direction, it.id),
-                          child: MovieListItem(
-                            id: it.id,
-                            title: it.title,
-                            broadcastAt: it.broadcastAt,
-                            onTap: () => _onTapProgram(context, it.id),
-                          )),
-                    )
-                    .toList();
-              }
-
-              return ListView(
-                children: children,
-              );
-            },
+              },
+            ),
           ),
         ),
       );
@@ -80,13 +96,11 @@ class _ScreenFcmState extends State<ScreenFcm> {
   void _onTapProgram(BuildContext context, String id) =>
       context.pushPage(GlobalRoutePath.program(id));
 
-  void _onDismissPrg(DismissDirection direction, String prgId) {
-    //todo implement
-  }
+  void _onDismissPrg(BuildContext context, String prgId) =>
+      context.read(_kPrvViewModel).unsubscribePrgFcmTopic(prgId);
 
-  void _onDismissChannel(DismissDirection direction, String channelId) {
-    //todo implement
-  }
+  void _onDismissChannel(BuildContext context, String channelId) =>
+      context.read(_kPrvViewModel).unsubscribeChannelFcmTopic(channelId);
 }
 
 @swidget
---
+++ lib/viewmodel/viewmodel_fcm.dart
@@ -0,0 +1,25 @@
+import 'package:shirasu/repository/hive_pref_repository.dart';
+import 'package:shirasu/viewmodel/message_notifier.dart';
+import 'package:shirasu/viewmodel/viewmodel_base.dart';
+import 'package:shirasu/screen_fcm/screen_fcm.dart';
+
+class ViewModelFcm extends ViewModelBase<String> {
+  ViewModelFcm(reader) : super(reader, '');
+
+  HivePrefRepository get _hiveRepo => reader(kPrvHivePrefRepository);
+
+  SnackBarMessageNotifier get _snackBarMsgNotifier =>
+      reader(kPrvFcmSnackBar);
+
+  void unsubscribeChannelFcmTopic(String id) {
+    _hiveRepo.unsubscribeChannelFcmTopic(id);
+    _commandSnack();
+  }
+
+  void unsubscribePrgFcmTopic(String id) {
+    _hiveRepo.unsubscribePrgFcmTopic(id);
+    _commandSnack();
+  }
+
+  void _commandSnack() => _snackBarMsgNotifier.notifyMsg(const SnackMsg.fcmUnsubscribe(), false);
+}

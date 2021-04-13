692f0a89ab8ff9abf345aa5368e721fc16c96a8b
HiroyukTamura <hirotamu3@gmail.com>
2021-04-13 15:35:52 +0900

    SHIR-103 # comment implement Sentry for web

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,14 @@ import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:shirasu/model/hive/fcm_topic.dart';
 import 'package:shirasu/model/hive/search_history.dart';
 import 'package:shirasu/repository/graphql_repository_impl.dart';
@@ -28,33 +30,46 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  await runZonedGuarded(() async {
-    await Hive.initFlutter();
-    await const EnvRepositoryImpl().load();
-    Hive
-      ..registerAdapter(HiveAuthDataAdapter())
-      ..registerAdapter(HiveBodyAdapter())
-      ..registerAdapter(HiveDecodedTokenAdapter())
-      ..registerAdapter(HiveClaimsAdapter())
-      ..registerAdapter(HiveHttpsShirasuIoUserAttributeAdapter())
-      // ..registerAdapter(HiveEncodedAdapter())
-      // ..registerAdapter(HiveHeaderAdapter())
-      ..registerAdapter(HiveUserAdapter())
-      ..registerAdapter(HiveFcmTopicAdapter())
-      ..registerAdapter(HiveFcmChannelDataAdapter())
-      ..registerAdapter(HiveFcmProgramDataAdapter())
-      ..registerAdapter(HiveSearchHistoryItemAdapter())
-      ..registerAdapter(HiveSearchHistoryAdapter());
+  await Hive.initFlutter();
+  await const EnvRepositoryImpl().load();
+  Hive
+    ..registerAdapter(HiveAuthDataAdapter())
+    ..registerAdapter(HiveBodyAdapter())
+    ..registerAdapter(HiveDecodedTokenAdapter())
+    ..registerAdapter(HiveClaimsAdapter())
+    ..registerAdapter(HiveHttpsShirasuIoUserAttributeAdapter())
+    // ..registerAdapter(HiveEncodedAdapter())
+    // ..registerAdapter(HiveHeaderAdapter())
+    ..registerAdapter(HiveUserAdapter())
+    ..registerAdapter(HiveFcmTopicAdapter())
+    ..registerAdapter(HiveFcmChannelDataAdapter())
+    ..registerAdapter(HiveFcmProgramDataAdapter())
+    ..registerAdapter(HiveSearchHistoryItemAdapter())
+    ..registerAdapter(HiveSearchHistoryAdapter());
 
-    await HiveAuthRepositoryImpl.instance().init();
-    await HivePrefRepositoryImpl.instance().init();
-    await HiveHistoryRepositoryImpl.instance().init();
-    await GraphQlRepositoryImpl.openHiveStore();
+  await HiveAuthRepositoryImpl.instance().init();
+  await HivePrefRepositoryImpl.instance().init();
+  await HiveHistoryRepositoryImpl.instance().init();
+  await GraphQlRepositoryImpl.openHiveStore();
 
-    runApp(
-      ProviderScope(child: MyApp()),
+  if (kIsWeb)
+    await SentryFlutter.init(
+      (options) {
+        options.dsn =
+            'https://bbd475d433724e46b8e2f342b35a107f@o327530.ingest.sentry.io/5672361';
+      },
+      appRunner: () => runApp(ProviderScope(
+        child: MyApp(),
+      )),
     );
-  }, FirebaseCrashlytics.instance.recordError);
+  else
+    await runZonedGuarded(
+        () async => runApp(
+              ProviderScope(
+                child: MyApp(),
+              ),
+            ),
+        FirebaseCrashlytics.instance.recordError);
 }
 
 class MyApp extends StatefulHookWidget {
